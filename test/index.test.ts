2b26456ccd41f173ca0680cf251e7dfb974c1862
SukkaW <isukkaw@gmail.com>
2026-02-26 01:52:08 +0800

    chore: replace `expect` w/ `earl`

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from 'mocha';
-import { expect } from 'expect';
+import { expect } from 'earl';
 import {
   prettyBandwidth, prettyBits, prettyTraffic,
   bits, bytes, ibits,
